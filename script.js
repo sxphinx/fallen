96bf5f5ce5e1a303a8a950059f1a70b717020c5c
sxphinx <42745717+sxphinx@users.noreply.github.com>
2021-09-21 09:44:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var character = document.getElementById("character");
+document.addEventListener("click",jump);
 
+function jump() {
+  if(character.classList == "animate"){return;}
+    character.classList.add("animate");
+    setTimeout(removeJump,300);
+};
+
+function removeJump() {
+    character.classList.remove("animate");
+}
