c0c2513fa5b87afa4fcd8b22e56494928c5ecd58
sxphinx <42745717+sxphinx@users.noreply.github.com>
2021-09-29 21:49:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-var trial = $('#trial'),
-    character = $('#character'),
-    w = trial.width() - character.width(),
+var pane = $('#trial'),
+    box = $('#character'),
+    w = pane.width() - box.width(),
     d = {},
     x = 3;
 
-// move function
 function newv(v,a,b) {
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > w ? w : n;
-  
-  $(window).keydown(function(e) { d[e.which] = true; });
-$(window).keyup(function(e) { d[e.which] = false; });
+  var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+  return n < 0 ? 0 : n > w ? w : n;
+}
+
+$(window).keydown(function(e) {
+  d[e.which] = true; 
+});
+$(window).keyup(function(e) {
+  d[e.which] = false;
+});
 
 setInterval(function() {
-    character.css({
-        left: function(i,v) { return newv(v, 37, 39); },
-        top: function(i,v) { return newv(v, 38, 40); }
-    });
+  box.css({
+    left: function(i,v) {
+      return newv(v, 37, 39); 
+    },
+    top: function(i,v) {
+      return newv(v, 38, 40); 
+    }
+  });
 }, 20);
 
 // jump function (not used)
